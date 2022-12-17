f06e2d745edf1ea94ec2627809ade7c7da13993c
Jonathan Kim <jonathankim980@gmail.com>
2022-12-17 04:00:48 -0600

    commit

---
+++ Sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'v77o2r84',
+    dataset: 'production'
+  }
+})
---
+++ frontend/src/container/About/About.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+import { AppWrap, MotionWrap } from '../../wrapper';
+import './About.scss';
+import { urlFor, client } from '../../client';
+
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
